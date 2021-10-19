f1a9bf45a56087ffc59273e452f5b71ea45bc9f5
MrHencke <hencke1@hotmail.com>
2021-10-19 16:39:48 +0200

    Update cors again

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from scripts.scale import scaleImage
 from flask import Flask
 app = Flask(__name__)
-CORS(app, resources={r"*": {"origins": "*"}},
-     methods=['GET', 'POST', 'PUT', 'DELETE'])
+CORS(app, support_credentials=True)
 
 
 @app.route('/api/ml', methods=['POST'])
+@cross_origin(origin='*')
 def runModel():
     return "placeholder"
 
 
 @app.route('/api/up', methods=['GET'])
+@cross_origin(origin='*')
 def isUpText():
     return "The server is up"
 
