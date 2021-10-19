c39758aecaf645e9eb288dc24cba5e9f90437c12
MrHencke <hencke1@hotmail.com>
2021-10-19 15:14:41 +0200

    fix?

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn server:app
+web: gunicorn server:create_app
--- server.py
+++ server.py
@@ -2,21 +2,21 @@ from flask import Flask, request
 from scripts.scale import scaleImage
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+def create_app():
+    app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.route('/api/ml', methods=['POST'])
-def runModel():
-    f = request.files['file']
-    #scaled = scaleImage(f)
-    #prediction = model.predict(scaled)
-    # return prediction
-    return "placeholder"
-    # Use route to process image, and return response.
+    @app.route('/api/ml', methods=['POST'])
+    def runModel():
+        f = request.files['file']
+        #scaled = scaleImage(f)
+        #prediction = model.predict(scaled)
+        # return prediction
+        return "placeholder"
+        # Use route to process image, and return response.
 
-
-@app.route('/api/up')
-def isUpText():
-    return "The server is up"
-    # Use route to check backend is up
+    @app.route('/api/up')
+    def isUpText():
+        return "The server is up"
+        # Use route to check backend is up
