662dcd5bf810955f3c04d7425fe8633e46d5a0a6
MrHencke <hencke1@hotmail.com>
2021-10-19 15:20:09 +0200

    fix?

--- server.py
+++ server.py
@@ -3,20 +3,25 @@ from scripts.scale import scaleImage
 from flask_cors import CORS
 
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-    @app.route('/api/ml', methods=['POST'])
-    def runModel():
-        f = request.files['file']
-        #scaled = scaleImage(f)
-        #prediction = model.predict(scaled)
-        # return prediction
-        return "placeholder"
-        # Use route to process image, and return response.
 
-    @app.route('/api/up')
-    def isUpText():
-        return "The server is up"
-        # Use route to check backend is up
+@app.route('/api/ml', methods=['POST'])
+def runModel():
+    f = request.files['file']
+    #scaled = scaleImage(f)
+    #prediction = model.predict(scaled)
+    # return prediction
+    return "placeholder"
+    # Use route to process image, and return response.
+
+
+@app.route('/api/up')
+def isUpText():
+    return "The server is up"
+    # Use route to check backend is up
+
+
+if __name__ == '__main__':
+    app.run()
