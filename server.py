8958751a05e9b6d56337db5e30fdca4ae7a30579
MrHencke <hencke1@hotmail.com>
2021-10-19 16:16:47 +0200

    Pls work

--- server.py
+++ server.py
@@ -1,18 +1,11 @@
-from flask import Flask, request
-#from scripts.scale import scaleImage
-#from flask_cors import CORS
+from flask import Flask
 
 app = Flask(__name__)
-
-#CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route('/api/ml', methods=['POST'])
 def runModel():
-    f = request.files['file']
-    #scaled = scaleImage(f)
-    #prediction = model.predict(scaled)
-    # return prediction
+
     return "placeholder"
 
 
