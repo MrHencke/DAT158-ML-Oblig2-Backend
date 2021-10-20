cce908cc13148f4e66aefede44776a286d595df6
MrHencke <hencke1@hotmail.com>
2021-10-20 16:08:11 +0200

    Fixed an oopsie

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -5,5 +5,5 @@ from PIL import Image, ImageOps
 def prepareImage(img):
     newimg = Image.open(img).convert('L').resize((28, 28))
     newimg = ImageOps.invert(newimg)
-    mnist = np.array(img).reshape(1, 28, 28)
+    mnist = np.array(newimg).reshape(1, 28, 28)
     return mnist / 255.0
